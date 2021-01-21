36d56b04357e2496f4fcc327646e7a0f8ad3f441
sowmya1408 <sowmyamannemr@gmail.com>
2021-01-21 14:00:00 +0100

    added getweatherbycity functionality in Homescreen.js

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -5,30 +5,30 @@ import { currentLocation } from "../context/currentLocationContext";
 import { View, Text, StyleSheet } from "react-native";
 
 export const HomeScreen = ({ navigation }: any) => {
-  // const [weatherData, setWeatherData] = useState(null);
-  // const [infoError, setInfoError] = useState();
-  // const [hourlyData, setHourlyData] = useState(null);
-  // const { displayAddress, lat, setLat, lon, setLon } = useContext(
-  //   currentLocation
-  // );
-  // const openSearch = useCallback(() => {
-  //   navigation.navigate("Search City");
-  // }, [displayAddress]);
-  // const getWeatherForCity = async () => {
-  //   try {
-  //     const weatherDataByCityName = await getWeatherByCityName(displayAddress);
-  //     setWeatherData(weatherDataByCityName);
-  //     setLat(weatherDataByCityName.coord.lat);
-  //     setLon(weatherDataByCityName.coord.lon);
-  //     const hourlyForcast = await forecasthourly(lat, lon);
-  //     setHourlyData(hourlyForcast);
-  //   } catch (err) {
-  //     setInfoError("Please enter valid city name");
-  //   }
-  // };
-  // useEffect(() => {
-  //   getWeatherForCity();
-  // }, [displayAddress]);
+  const [weatherData, setWeatherData] = useState(null);
+  const [infoError, setInfoError] = useState();
+  const [hourlyData, setHourlyData] = useState(null);
+  const { displayAddress, lat, setLat, lon, setLon } = useContext(
+    currentLocation
+  );
+  const openSearch = useCallback(() => {
+    navigation.navigate("Search City");
+  }, [displayAddress]);
+  const getWeatherForCity = async () => {
+    try {
+      const weatherDataByCityName = await getWeatherByCityName(displayAddress);
+      setWeatherData(weatherDataByCityName);
+      setLat(weatherDataByCityName.coord.lat);
+      setLon(weatherDataByCityName.coord.lon);
+      const hourlyForcast = await forecasthourly(lat, lon);
+      setHourlyData(hourlyForcast);
+    } catch (err) {
+      setInfoError("Please enter valid city name");
+    }
+  };
+  useEffect(() => {
+    getWeatherForCity();
+  }, [displayAddress]);
 
   // const getWeatherHourly = async () => {
   //   try {
