813a6f9d3c3b099c5051a5323ebe2b6b9c5068ec
sowmya1408 <sowmyamannemr@gmail.com>
2021-02-04 17:00:00 +0100

    added Hourly UI component

---
+++ src/components/Hourly.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { View, Text, FlatList, StyleSheet, Image } from "react-native";
+
+export const Hourly = ({ hourlyDetails }: any) => {
+  const renderItem = ({ item }: any) => {
+    const milliseconds = item?.dt * 1000;
+    const time = new Date(milliseconds).getHours();
+    return (
+      <View style={listStyles.listText}>
+        <Text style={listStyles.text}>{time}:00</Text>
+        <Text style={listStyles.text}>{(item.temp - 273).toFixed(0)}</Text>
+        <Image
+          style={listStyles.iconImage}
+          source={{
+            uri: `http://openweathermap.org/img/wn/${item?.weather[0]?.icon}@4x.png`,
+          }}
+        />
+      </View>
+    );
+  };
+  return (
+    <FlatList
+      bounces={false}
+      horizontal={true}
+      data={hourlyDetails?.hourly}
+      renderItem={renderItem}
+      keyExtractor={(item, i) => `${item} is ${i}`}
+      showsHorizontalScrollIndicator={true}
+    />
+  );
+};
+
+const listStyles = StyleSheet.create({
+  listText: {
+    justifyContent: "flex-end",
+  },
+  text: {
+    color: "white",
+  },
+  iconImage: {
+    width: 50,
+    height: 50,
+  },
+});
